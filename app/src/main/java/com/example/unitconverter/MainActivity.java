23554fcb0ba0c2711ff1ab78521bec1f20f5da4d
CastielM <78852960+CastielM@users.noreply.github.com>
2023-04-12 00:45:20 +1000

    Basic working conversions, some improvements to be made

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -46,17 +46,81 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                String selectedTo = convertTo.getSelectedItem().toString();
+                String selectedFrom = convertFrom.getSelectedItem().toString();
 
+                //checks that input value is not blank
                 if (inputUnit.getText().toString().length() == 0)
                 {
                    Toast.makeText(MainActivity.this , "Must enter a value to convert", Toast.LENGTH_LONG).show();
                 }
+
+                //checks that the selected item for conversion units are not index 0 (aka an invalid hint value)
+                else
+                if (convertFrom.getSelectedItemPosition() == 0)
+                {
+                    Toast.makeText(MainActivity.this , "Must choose a unit to convert from", Toast.LENGTH_LONG).show();
+                }
+                else
+                if (convertTo.getSelectedItemPosition() == 0)
+                {
+                    Toast.makeText(MainActivity.this , "Must choose a unit to convert to", Toast.LENGTH_LONG).show();
+                }
+
+                //checks for varying conversion cases
+                else
+                if (selectedFrom.equals("Celsius") && selectedTo.equals("Fahrenheit"))
+                {
+                    double result = Double.parseDouble(inputUnit.getText().toString());
+                    result = (result * 1.8) + 32;
+                    resultText.setText(String.valueOf(result));
+                }
+                else
+                if (selectedFrom.equals("Fahrenheit") && selectedTo.equals("Celsius"))
+                {
+                    double result = Double.parseDouble(inputUnit.getText().toString());
+                    result = (result - 32) / 1.8;
+                    resultText.setText(String.valueOf(result));
+                }
+                else
+                if (selectedFrom.equals("Kelvin") && selectedTo.equals("Fahrenheit"))
+                {
+                    double result = Double.parseDouble(inputUnit.getText().toString());
+                    result = ((result - 273.15) * 1.8) + 32;
+                    resultText.setText(String.valueOf(result));
+                }
+                else
+                if (selectedFrom.equals("Fahrenheit") && selectedTo.equals("Kelvin"))
+                {
+                    double result = Double.parseDouble(inputUnit.getText().toString());
+                    result = ((result - 32) / 1.8) + 273.15;
+                    resultText.setText(String.valueOf(result));
+                }
+                else
+                if (selectedFrom.equals("Kelvin") && selectedTo.equals("Celsius"))
+                {
+                    double result = Double.parseDouble(inputUnit.getText().toString());
+                    result = result - 273.15;
+                    resultText.setText(String.valueOf(result));
+                }
+                else
+                if (selectedFrom.equals("Celsius") && selectedTo.equals("Kelvin"))
+                {
+                    double result = Double.parseDouble(inputUnit.getText().toString());
+                    result = result + 273.15;
+                    resultText.setText(String.valueOf(result));
+                }
+
+                //if none of the above cases, through an error, at the moment should throw if you choose to convert to the same unit
                 else
                 {
-                    int unit = Integer.parseInt(inputUnit.getText().toString());
-                    unit = unit * 3;
-                    resultText.setText(String.valueOf(unit));
+                    Toast.makeText(MainActivity.this , "Something went wrong", Toast.LENGTH_LONG).show();
                 }
+
+
+
+
+
             }
         });
 
